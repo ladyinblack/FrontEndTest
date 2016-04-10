4a6005bb19895e4a621153c11feb238d86b3af9a
Ronica Singh <ladyinblack2010rs@gmail.com>
2016-04-10 19:45:22 +0200

    Update listlocations.js

--- listlocations.js
+++ listlocations.js
@@ -1,40 +1,72 @@
-window.onload = function() {
-	var map = document.getElementById('content');
+// Create a Foursquare developer account
+// https://developer.foursquare.com/
+var config = {
+	CLIENT_ID: '0R1X4PD1OAA55OIV0HEX0YZZZZO3SMWBY1VXZFTNQFUPBKRG',
+	CLIENT_SECRET: 'W2EKI2P5O3DU22KYR31NDAULZUOMJLELHK0DZRMSFUDUTU1B',
+	REQUEST_DATE: new Date().toISOString().slice(0,10).replace(/-/g, ""), 	// returns current date in format YYYYMMDD
+	API_URL: 'https://api.foursquare.com/v2/'
+}
+
+// HTML5 Geolocation
+navigator.geolocation.getCurrentPosition(function (data) {
+	var content = document.getElementById('content');
+	var lat = data['coords']['latitude'];
+	var lng = data['coords']['longitude'];
 	
-	/* HTML5 Geolocation */
-	navigator.geolocation.getCurrentPosition(function (data) {
-		var lat = data['coords']['latitude'];
-		var lng = data['coords']['longitude'];
-		
-		/* Create a Foursquare developer account */
-		/* https://developer.foursquare.com/ */
-		var CLIENT_ID = '0R1X4PD1OAA55OIV0HEX0YZZZZO3SMWBY1VXZFTNQFUPBKRG';
-		var CLIENT_SECRET = 'W2EKI2P5O3DU22KYR31NDAULZUOMJLELHK0DZRMSFUDUTU1B';
-		var REQUEST_DATE = new Date().toISOString().slice(0,10).replace(/-/g, "");		// returns current date in format YYYYMMDD
-		var API_URL = 'https://api.foursquare.com/v2/venues/search';
-		
-		/* https://developer.foursquare.com/start/search/ */
-		var API_ENDPOINT = API_URL + '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + REQUEST_DATE + '&ll=' + lat + ',' + lng;
-		
-		// Make an AJAX request to Foursquare 
-		// to load markers data.
-		var request = new XMLHttpRequest();
-		request.open("GET", API_ENDPOINT, true);
-		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.onreadystatechange = function() {
-			if (request.readyState == 4 && request.status == 200) {
-				data = JSON.parse(request.responseText);
-				venues = data["response"]["venues"];
-				map.innerHTML = "";
+	// https://developer.foursquare.com/start/search/
+	var endpoint = 'venues/search';
+	var apiUrl = config.API_URL + endpoint + '?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=' + config.REQUEST_DATE + '&ll=' + lat + ',' + lng;
+	
+	// Make an AJAX request to Foursquare
+	var request = new XMLHttpRequest();
+	request.open("GET", apiUrl, true);
+	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			data = JSON.parse(request.responseText);
+			var venues = data["response"]["venues"];
+			
+			content.innerHTML = "";
+			for (var i = 0; i < venues.length; i++) {
+				content.innerHTML += "<a href=\"#\"" + " id=\"locationLinks\"" + " data-locationId=\"" + venues[i].id + "\"" + ">" + venues[i].name + "</a><hr />";
+//				(function () {
+					var vid = document.getElementById("locationLinks");
+					vid.addEventListener("click", getPhotos);
+//				}());
 				
-				// Transform each venue result into a marker on the map.
-				for (var i = 0; i < venues.length; i++) {
-					map.innerHTML += "<a href=\"view/" + venues[i].id + "\">" + venues[i].name + "</a><hr />";
-				}
 			}
 		}
-		request.send(null);
-		map.innerHTML = "requesting...";
-	});
+	}
+	request.send(null);
+	content.innerHTML = "requesting...";
+	//content.innerHTML = "Latitude: " + lat + "<br />Longitude: " + lng;
+});
+
+function getPhotos(e) {
+	var venueId = document.getElementById("locationLinks").getAttribute("data-locationId");
+//	view.innerHTML = venueId;
+	var endpoint = 'venues/' + venueId + '/photos?';
+	var apiUrl = config.API_URL + endpoint + 'client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=' + config.REQUEST_DATE;
 	
+	var view = document.getElementById('view');
+	
+	var request = new XMLHttpRequest();
+	request.open("GET", apiUrl, true);
+	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			var data = JSON.parse(request.responseText);
+			var photos = data["response"]["photos"]["items"];
+			view.innerHTML = "";
+			
+			for (var i = 0; i < photos.length; i++) {
+				view.innerHTML += "<a href=\"" + photos[i].prefix + photos[i].width + "x" + photos[i].height + photos[i].suffix + "\"/>" + photos[i].suffix + "</a><hr />";
+			}
+		}
+	}
+	request.send(null);
+	view.innerHTML = "requesting...";
+	
+	e.preventDefault();
 }
